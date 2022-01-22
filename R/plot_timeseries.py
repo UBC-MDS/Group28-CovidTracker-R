fa08d6d474a0b754b74cf8c8676253a9f65cca5d
SiqiTao <52697729+SiqiTao@users.noreply.github.com>
2022-01-22 01:33:25 -0800

    Create plot_timeseries.py

---
+++ R/plot_timeseries.py
@@ -0,0 +1,13 @@
+#' Create a plot of the number of covid cases over time.
+#'
+#' @param covid_df a dataframe containing covid data to plot
+#' @param metric the name of column from covid data to plot along time
+#'
+#' @return plot object
+#' @export
+#'
+#' @examples
+#' plot_geographical(df,'cases')
+plot_timeseries <- function(covid_df, metric) {
+
+}
