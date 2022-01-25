f0f4df23f5bad2a2d55dfd70120230291fa116a6
Cuthbert Chow <cuthbert.chow@gmail.com>
2022-01-25 14:40:33 -0800

    Update get_covid_data() With Tests

--- R/get_covid_data.R
+++ R/get_covid_data.R
@@ -1,3 +1,7 @@
+library(tidyverse)
+library(httr)
+library(jsonlite)
+
 #' Acquires Canada Covid data of specified type
 #' and for an optionally provided date and province
 #'
@@ -10,6 +14,35 @@
 #'
 #' @examples
 #' get_covid_data('cases', 'BC', '13-01-2021')
-get_covid_data <- function(data_type, loc, date) {
-    
+get_covid_data <- function(data_type="cases", loc="", date="") {
+  
+ 
+  stat_types <- c('cases', 'mortality', 'recovered', 'testing', 'active',
+                'dvaccine', 'avaccine', 'cvaccine')
+  provinces <- c('AB', 'BC', 'MB', 'NB', 'NL', 'NT', 'NS', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT', 'RP', '')
+  
+  if (! data_type %in% stat_types){
+    stop("Stat type must be within the pre-defined options")
+  }
+  if (! loc %in% provinces){
+    stop("Provinces must be within pre-defined options or left empty.")
+  }
+  if (is.na(str_extract(date, regex("(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-\\d{4}|$^")))){
+    stop("Input date must be string following the DD-MM-YYYY format")
+  }
+
+
+  endpoint <- "https://api.opencovid.ca/timeseries"
+  params <- list(stat = data_type, loc = loc, date = date)
+  
+  res = GET(endpoint, query = params)
+  return(data.frame(fromJSON(rawToChar(res$content))[[data_type]]))
 }
+
+df <- get_covid_data("cases", "", "25-08-2020")
+
+nrow(df)
+ncol(df)
+
+
+
