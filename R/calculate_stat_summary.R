5504442a883b7029950070cd25e414d9b7b54e26
Tianwei Wang <wangtianwei@hotmail.com>
2022-01-27 15:34:19 -0800

    Add calculate_stat_summary.R file

--- R/calculate_stat_summary.R
+++ R/calculate_stat_summary.R
@@ -1,11 +1,13 @@
 #' Creates summary information about the covid cases in each province of Canada
 #'
-#' @param df: data.frame containing covid data to summary
+#' @param df: data.frame containing covid data to summarize
+#' @param column: the column name to summarize
 #'
 #' @return data.frame containing summary information.
 #' @export
 #'
 #' @examples
-#' calculate_stat_summary(covid_df)
-calculate_stat_summary <- function(df) {
+#' calculate_stat_summary(covid_df, 'cases')
+calculate_stat_summary <- function(df, column) {
+
 }
