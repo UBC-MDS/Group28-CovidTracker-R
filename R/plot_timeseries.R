aab488b8e8d9dfcf56eb2d6ae10ebc8e020d9270
Siqi Tao <taosiqi728@163.com>
2022-01-28 08:51:45 -0800

    Add time series

---
+++ R/plot_timeseries.R
@@ -0,0 +1,92 @@
+#' Create a plot of the number of covid cases over time.
+#'
+#' @param covid_df a dataframe containing covid data to plot
+#' @param metric the name of column from covid data to plot along time
+#' @param start a string indicating the start date in pattern YYYY-MM-DD
+#' @param end a string indicating the end date in pattern YYYY-MM-DD
+#'
+#' @return plot object
+#' @export
+#'
+#' @examples
+#' plot_timeseries(df,'cases')
+
+library(tidyverse)
+library(lubridate)
+library(ggplotly)
+
+plot_timeseries <- function(covid_df, metric, start = '', end = '') {
+    
+    # Test input values
+    if(!is.data.frame(covid_df)) {
+        stop("The datatype of covid_df must be data frame.")
+    }
+    
+    if(!is.character(metric)) {
+        stop("The datatype of metric must be string.")
+    }
+    
+    if(!is.character(start)) {
+        stop("The datatype of start must be string.")
+    }
+    
+    if(!is.character(end)) {
+        stop("The datatype of end must be string.")
+    }
+    
+    if(!(metric %in% colnames(covid_df))) {
+        stop("Chosen metric must be a column of the data.")
+    }
+    
+    if((str_detect(metric, "date")) or (str_detect(metric, "province"))) {
+        stop("Chosen metric must not be date or province..")
+    }
+    
+    if(metric == "testing_info") { 
+        stop("This column is not available for plotting, please choose another column.")
+    }
+    
+    # Find and convert the date column
+    for (i in colnames(covid_df)) {
+        if(str_detect(i, "date")) {
+            covid_df$new_date <- dmy(covid_df[,i])
+        }
+    }
+    
+    pattern <- "^\\d{4}-\\d{2}-\\d{2}$"
+    
+    if((start != "") and (end != "")){
+        if(!str_detect(start, pattern)) {
+            stop("Input start date should be in pattern YYYY-MM-DD")
+        } else if(!str_detect(end, pattern)) {
+            stop("Input end date should be in pattern YYYY-MM-DD")
+        } else {
+            start <- ymd(start)
+            end <- ymd(end)
+        }
+        
+        if(end <= start) {
+            stop("The start date must be before the end date.")
+        }
+    } 
+    
+    if(start == "") {
+        start <- min(covid_df$new_date)
+    }
+    
+    if(end == "") {
+        start <- max(covid_df$new_date)
+    }
+    
+    data <- select(covid_df, {{metric}}, new_date) |> filter(new_date <= end, new_date >= start)
+    
+    plot <- ggplot(data, aes(new_date, metric)) + 
+        geom_line() +
+        xlab("Date") +
+        ylab(sprintf("%s",str_to_title(str_replace(metric,"_"," ")))) +
+        ggtitle(Time series plot of sprintf("%s",str_to_title(str_replace(metric,"_"," "))))
+    
+    plot <- ggplotly(plot)
+    
+    return (plot)
+}
